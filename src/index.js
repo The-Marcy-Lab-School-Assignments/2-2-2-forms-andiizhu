29af16132ee96f22f13f220007472223c6633b19
andiizhu <azhu0317@gmail.com>
2024-06-04 19:01:26 -0400

    finished assignment

--- src/index.js
+++ src/index.js
@@ -2,5 +2,25 @@
  *
  */
 
-const handleSubmit = () => {
+const handleSubmit = (event) => {
+  event.preventDefault();
+  const form = event.target;
+  const username = form.username.value;
+  const codingLevel = form.codingLevel.value;
+  const location = form.location.value;
+  const didLikeAssignment = form.didLikeAssignment.checked;
+
+  document.querySelector("#results-username").textContent = username;
+  document.querySelector("#results-coding-level").textContent = codingLevel;
+  document.querySelector("#results-location").textContent = location;
+  const resultLikeAssignment = document.querySelector(
+    "#results-did-like-assignment"
+  );
+  if (didLikeAssignment) {
+    resultLikeAssignment.textContent = "Yes";
+  } else resultLikeAssignment.textContent = "No";
+
+  form.reset();
 };
+
+document.querySelector("form").addEventListener("submit", handleSubmit);
